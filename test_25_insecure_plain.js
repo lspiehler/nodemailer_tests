7804a1d5b002bcbc59c82040a2c46c6d6daf5a90
Lyas Spiehler <ls12943@netconf.smhplus.org>
2021-01-16 11:33:49 -0600

    first commit

---
+++ test_25_insecure_plain.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+var path = require('path');
+var scriptName = path.basename(__filename);
+var scriptDir = path.dirname(__filename);
+var fs = require('fs');
+
+var nodemailertransportparams = {
+		host: process.env.SMTPSERVER,
+		port: 25,
+		ignoreTLS: true
+}
+
+var nodemailermailoptions = {
+		from: process.env.FROM, // sender address
+		to: [process.env.TO], // list of receivers
+		subject: scriptName,
+		auth: {
+				method: 'PLAIN',
+				user: process.env.USERNAME,
+				pass: process.env.PASSWORD
+		}
+}
+
+var transporter = nodemailer.createTransport(
+			nodemailertransportparams
+		);
+	
+fs.readFile(scriptDir + '/' + scriptName, 'utf8', function(err, contents) {	
+	var mailOptions = nodemailermailoptions
+
+	mailOptions.text = contents.toString();
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('success');
+		}
+	});
+});
---
+++ test_25_secure_no_auth.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer');
+var path = require('path');
+var scriptName = path.basename(__filename);
+var scriptDir = path.dirname(__filename);
+var fs = require('fs');
+
+var nodemailertransportparams = {
+		host: process.env.SMTPSERVER,
+		port: 25,
+		tls: {rejectUnauthorized: false},
+		requireTLS: true
+}
+
+var nodemailermailoptions = {
+		from: process.env.FROM, // sender address
+		to: [process.env.TO], // list of receivers
+		subject: scriptName
+}
+
+var transporter = nodemailer.createTransport(
+			nodemailertransportparams
+		);
+	
+fs.readFile(scriptDir + '/' + scriptName, 'utf8', function(err, contents) {	
+	var mailOptions = nodemailermailoptions
+
+	mailOptions.text = contents.toString();
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('success');
+		}
+	});
+});
